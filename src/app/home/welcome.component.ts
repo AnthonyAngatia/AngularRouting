9f68ddfa3ae4d8197e0565b78f9e9c8052c39ffd
unknown <anthonyangatia@gmail.com>
2022-05-19 11:47:28 +0300

    Routing basics

--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+/*
+* A selector is only used if the component will be embedded in another component.
+* The selector is used as a directive
+* */
 @Component({
   selector: 'pm-home',
   templateUrl: './welcome.component.html'

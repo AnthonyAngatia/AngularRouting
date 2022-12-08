2b9fd85daeeab0dcb84db0caf5f4b081fc170b99
anthony angatia <anthonyangatia@gmail.com>
2022-12-08 10:21:44 +0300

    [Feature]Can load with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,20 @@ import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {WelcomeComponent} from './home/welcome.component';
 import {PageNotFoundComponent} from './page-not-found.component';
+import {AuthGuard} from './user/auth.guard';
 
 @NgModule({
   imports: [RouterModule.forRoot([
     {path: 'welcome', component: WelcomeComponent},
     {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+    {
+      path: 'products',
+      canActivate: [AuthGuard],
+      loadChildren: () => {
+        return import('./products/product.module').then(m => m.ProductModule);
+      }
+    },
+
     {path: '**', component: PageNotFoundComponent}
   ])
   ],
